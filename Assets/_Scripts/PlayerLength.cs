f497abe67e9a1e3549086acff8a2c3d83822b848
Singh-Ishu <itzshaansingh@gmail.com>
2025-04-01 13:29:18 +0530

    Added a tail manipulation script

--- Assets/_Scripts/PlayerLength.cs
+++ Assets/_Scripts/PlayerLength.cs
@@ -1,8 +1,24 @@
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerLength : NetworkBehaviour
 {
-    public NetworkVariable<ushort> length = new(1);
+    [SerializeField] private GameObject tailPrefab;
 
+    public NetworkVariable<ushort> length = new(value:1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+    private List<GameObject> _tails;
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        _tails = new List<GameObject>();
+    }
+
+    private void InstantiateTail()
+    {
+        GameObject tail = Instantiate(tailPrefab, transform.position, Quaternion.identity);
+        _tails.Add(tail);
+    }
 }
---
+++ Assets/_Scripts/Tail.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Tail : MonoBehaviour
+{
+    public Transform networkedOwner;
+    public Transform followTransform;
+
+    [SerializeField] private float delayTime = 0.1f;
+    [SerializeField] private float distance = 0.3f;
+    [SerializeField] private float moveStep = 10f;
+
+    private Vector3 _targetPosition;
+
+    private void Update()
+    {
+        _targetPosition = followTransform.position - followTransform.forward * distance;
+        _targetPosition += (transform.position - _targetPosition) * delayTime;
+        _targetPosition.z = 0;
+
+        transform.position = Vector3.Lerp(a: transform.position, b: _targetPosition,t:Time.deltaTime * moveStep);
+    }
+}
