a518c38519bb3ebd32e49dd255ecb8f2155f633d
Singh-Ishu <itzshaansingh@gmail.com>
2025-04-01 13:43:08 +0530

    Added tail length increase

--- Assets/_Scripts/PlayerLength.cs
+++ Assets/_Scripts/PlayerLength.cs
@@ -9,16 +9,34 @@ public class PlayerLength : NetworkBehaviour
     public NetworkVariable<ushort> length = new(value:1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
 
     private List<GameObject> _tails;
+    private Transform _lastTail;
+    private Collider2D _collider2D;
 
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         _tails = new List<GameObject>();
+        _lastTail = transform;
+        _collider2D = GetComponent<Collider2D>();
     }
 
+    [ContextMenu(itemName:"AddLength")]
+    private void AddLength()
+    {
+        length.Value += 1;
+        InstantiateTail();
+    }
     private void InstantiateTail()
     {
-        GameObject tail = Instantiate(tailPrefab, transform.position, Quaternion.identity);
-        _tails.Add(tail);
+        GameObject tailGameObject = Instantiate(tailPrefab, transform.position, Quaternion.identity);
+        tailGameObject.GetComponent<SpriteRenderer>().sortingOrder = -length.Value;
+        if (tailGameObject.TryGetComponent(out Tail tail))
+        {
+            tail.networkedOwner = transform;
+            tail.followTransform = _lastTail;
+            _lastTail = tailGameObject.transform;
+            Physics2D.IgnoreCollision(tail.GetComponent<Collider2D>(),_collider2D);
+        }
+        _tails.Add(tailGameObject);
     }
 }
